c2673c9e6a531d844cfd07ae87bc44c8d4d666c2
Sisanda Masango <sisandalisamasango@gmail.com>
2025-07-29 03:32:18 +0200

    1st task

--- Practice.py
+++ Practice.py
@@ -1,23 +1,28 @@
-x = 121
-x_str = str(x)
-x_len = len(str(x))
-x_list = [x]
-reverse = ""
+def calculate_love_score(name1, name2):
+    name_one = name1.upper()
+    name_two = name2.upper()
+    total_for_true = ""
+    total_for_love = ""
 
-
-# for i in x_len:
-# print(x_len[-1])
-# print(x_len[-2])
-# print(x_len[-3])
-
-
-for i in range(1, x_len + 1):
-    reverse += x_str[-i]
-if x_str == reverse:
-    print("Palindrome")
-else:
-    print("Not Palindrome")
- 
-print(reverse)
-
+    for i in name_one:
+        for j in "TRUE":
+            if i == j:
+                total_for_true += i
+        for k in "LOVE":
+            if i == k:
+                total_for_love += i 
+          
     
+    for h in name_two:
+        for m in "TRUE":
+            if h == m:
+                total_for_true += h
+        for n in "LOVE":
+            if h == n:
+                total_for_love += n
+    total = (str(len(total_for_true)) + str(len(total_for_love)))
+    print(total)
+   
+                
+
+calculate_love_score("Thandoluhle","Lisa")
