56fbfb7981facc1d7c3a062ee6b0459502048061
Sisanda Masango <sisandalisamasango@gmail.com>
2025-07-30 21:04:04 +0200

    I still need to fix the decoding error

--- caesar_cypher.py
+++ caesar_cypher.py
@@ -1,34 +1,39 @@
+
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
+def caesar(original_text, shift_amount, encode_or_decode):
+    output_text = ""
 
-def caesar(direction):
-    if direction == "decrypt":
+    for letter in original_text:
+        if letter not in alphabet:
+            output_text += letter
+        else:
+            if encode_or_decode == "decode":
+                shift_amount *= -1
+            shifted_position = alphabet.index(letter) + shift_amount
+            shifted_position %= len(alphabet)
+            output_text += alphabet[shifted_position]
+    print(f"Here is the {encode_or_decode}d result: {output_text}")
 
-        def decrypt(original_text, shift_amount):
-            decrypt_text = ""
-            for letter in original_text:
-                shifted_position = alphabet.index(letter) - shift_amount
-                decrypt_text += alphabet[shifted_position]
-            print(f"Here is the encoded result: {decrypt_text}")
-        decrypt(text, shift)
+def questions():
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    caesar(original_text=text, shift_amount=shift, encode_or_decode=direction)
+questions()
 
-    elif direction == "encode":
-
-
-        def encrypt(original_text, shift_amount):
-            cipher_text = ""
-            for letter in original_text:
-                shifted_position = alphabet.index(letter) + shift_amount
-                shifted_position %= len(alphabet)
-                cipher_text += alphabet[shifted_position]
-            print(f"Here is the encoded result: {cipher_text}")
-        encrypt(text, shift)
+game_over = True
+while game_over:
+    question = input("Do you want to go again? ")
+    if question == "yes":
+        questions()
+    elif question == "no":
+        game_over = False
     else:
-        print("Try Again")
-caesar(direction)
+        print("Type Valid answer between yes and no.")
+        questions()
+
+
 
 
 
